04a7962d02e491622d56b87057c6aef0362ed750
morchix13 <maenchit17@gmail.com>
2026-02-09 19:52:01 +0700

    Add files via upload

---
+++ cell02/ex03/mult.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+a = int(input())
+b = int(input())
+
+result = a * b
+
+if result > 0:
+    print("The result is positive.")
+elif result < 0:
+    print("The result is negative.")
+else:
+    print("The result is zero.")
+
+print(result)
